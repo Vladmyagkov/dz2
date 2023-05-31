369e3b9667e3137cdccb2c97a6e31a9755c894bd
Vladislav <Vladmyagkov64@yandex.ru>
2023-05-31 12:40:10 +0300

    Сделал дз 3, но так и не понял с полиндромом

---
+++ dz3/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.WriteLine("Введите координату X1");
+// int X1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y1");
+// int Y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Z1");
+// int Z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату X2");
+// int X2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y2");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Z2");
+// int Z2 = Convert.ToInt32(Console.ReadLine());
+
+// double b = Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2)+Math.Pow((Z2-Z1),2));
+
+// Console.WriteLine(b);
+
+
+// _____________________________________________________________
+
+
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count =1;
+
+// while (num>=count)
+// {
+//     Console.Write(Math.Pow((count),3)+" ");
+//     count++;
+// }
